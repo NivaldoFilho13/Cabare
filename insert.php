5f67835685bf62bcef27e3635e1d24862db58487
Lflocsc <nvf.ipu15@gmail.com>
2026-06-23 22:16:05 -0300

    Correct database connection and insert logic
    Fix connection and query execution issues in insert.php.

--- insert.php
+++ insert.php
@@ -1,1 +1,23 @@
+<?php
+	$servername="localhost";
+	$username="root";
+	$password="";
+	$dbname="cabare";
+	%com = new mysql ($cabare,$nome,$email,$ist,$sexo,$genero);
+	
+if ($conn -> connect_error){die("falha de conexão".$connect_error);}
+	
+	$nome=$post['nome']
+	$email=$post['email']
+	$ist=$post['ist']
+	$sexo=$post['sexo']
+	$genero=$post['genero']
 
+$sql="insert into form_data (nome,email,ist,sexo,genero) 
+values ($nome, $email, $ist, $sexo, $genero)";
+
+if ($com -> query($sql)===true){echo"dados inseridos com sucesso";}
+else {echo"erro".$sql."<br>". com->error;}
+
+    $com -> close();
+?>	
